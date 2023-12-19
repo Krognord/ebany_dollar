8822b89b70290d0a160e05c01b44e8656a7e6a32
Krognord <fufvtvyjy1@gmail.com>
2023-12-19 20:39:10 +0300

    SOSAT'!!!!!!!!!!

--- мой_код.py
+++ мой_код.py
@@ -1,39 +1,47 @@
 import requests
+import matplotlib.pyplot as plt
 
-def get_historical_dollar_prices(api_key, start_date, end_date):
-    base_url = "https://api.freecurrencyapi.com/v1/historical"
-    base_currency = "USD"
-    target_currency = "RUB"
+api_key = 'fca_live_a7wJo65vWDCC6bTs7magJRxY7ciZt6vCq7tWtLhk'
+base_url = 'https://api.freecurrencyapi.com/v1/historical'
+base_currency = 'USD'
+target_currency = 'RUB'
+dates = ['2023-01-01', '2023-02-02', '2023-04-04', '2023-06-06', '2023-08-08', '2023-10-10', '2023-12-12']
 
-    headers = {
-        "apikey": api_key
-    }
+result_list = []
 
+for date in dates:
     params = {
-        "base_currency": base_currency,
-        "target_currency": target_currency,
-        "start_date": start_date,
-        "end_date": end_date
+        'apikey': api_key,
+        'base_currency': base_currency,
+        'target_currency': target_currency,
+        'date': date
     }
 
-    response = requests.get(base_url, headers=headers, params=params)
+    response = requests.get(base_url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        return data
+        try:
+            # Получаем стоимость 1 доллара в рублях
+            price = data['data'][date][target_currency]
+            result_list.append((date, price))
+        except KeyError:
+            print(f'Ошибка при извлечении данных для даты {date}.')
     else:
-        print(f"Error: {response.status_code}")
-        return None
+        print(f'Ошибка при запросе данных для даты {date}. Код ответа: {response.status_code}')
 
-# Example usage with multiple dates
-api_key = "fca_live_a7wJo65vWDCC6bTs7magJRxY7ciZt6vCq7tWtLhk"
-date_list = ["2023-01-01", "2023-04-04", "2023-07-07", "2023-10-10"]
+# Извлекаем даты и цены для построения графика
+dates, prices = zip(*result_list)
 
-for date in date_list:
-    historical_data = get_historical_dollar_prices(api_key, date, date)
+# Построение графика
+plt.figure(figsize=(10, 6))
+plt.plot(dates, prices, marker='o')
+plt.title(f'Стоимость 1 {base_currency} в {target_currency}')
+plt.xlabel('Дата')
+plt.ylabel('Цена')
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.tight_layout()
 
-    if historical_data:
-        print(f"Historical data for {date}: {historical_data}")
-        # Process the historical data as needed
-    else:
-        print(f"Failed to retrieve historical data for {date}.")
+# Отображение графика
+plt.show()
